f0fbbba45c82ad4407d738f99d1ab4185469a95e
brendon.thiede@gmail.com <brendon.thiede@gmail.com@acd44a81-f779-c8b6-c2a5-faa3f53079dd>
2011-05-16 03:24:23 +0000

    Initial commit

---
+++ grails-app/controllers/org/example/DeveloperController.groovy
@@ -0,0 +1,6 @@
+package org.example
+
+class DeveloperController {
+
+    def scaffold = Developer
+}
---
+++ grails-app/domain/org/example/Developer.groovy
@@ -0,0 +1,12 @@
+package org.example
+
+class Developer {
+    String name
+    int birthAge
+    int javaAge
+    
+    static constraints = {
+        name(blank: false)
+        javaAge(nullable: false)
+    }
+}
